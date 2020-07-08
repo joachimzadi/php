1c8d78d786ece13005b46daa6f722bfab1dfa690
J4LPROJECTS <gitprojects@j4ltechnologies.com>
2020-07-08 13:58:05 +0200

    Commit Formulaire

--- cours/04-LesConstantes/index.php
+++ cours/04-LesConstantes/index.php
@@ -10,3 +10,17 @@ echo "<h1>" . MSG . "</h1>";
 echo "<h1>" . mSg . "</h1>";
 
 const UNE_CONSTANTE = "UNE CONSTANTE";
+//UNE_CONSTANTE = "UNE CONSTANTE";//INTERDIT
+
+//LES CONSTANTES MAGIQUES
+echo "<h1>Nous sommes la ligne " . __LINE__ . "</h1>";
+echo "<h1>Je suis le fichier " . __FILE__ . "</h1>";
+echo "<h1>Je suis le repertoire " . __DIR__ . "</h1>";
+
+function une_fonction()
+{
+       echo "<h1>Je suis fonction " . __FUNCTION__ . "</h1>";
+}
+
+une_fonction();
+
