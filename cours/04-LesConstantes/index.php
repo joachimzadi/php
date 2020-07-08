baa2c2f2ade52e88896fdb0e2c88c40d0f47c0c1
J4LPROJECTS <gitprojects@j4ltechnologies.com>
2020-07-08 10:28:05 +0200

    Commit initial

---
+++ cours/04-LesConstantes/index.php
@@ -0,0 +1,12 @@
+<?php
+
+define("MESSAGE", "Bonjour MESSAGE PHP");//Case sensitive
+
+echo "<h1>" . MESSAGE . "</h1>";
+
+define("MSG", "Bonjour MSG PHP", true);//Case insensitive
+
+echo "<h1>" . MSG . "</h1>";
+echo "<h1>" . mSg . "</h1>";
+
+const UNE_CONSTANTE = "UNE CONSTANTE";
